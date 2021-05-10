dbe82c0a1e4ad89bc46eeb8fd9f93cca100bdaaf
Olowookere Marvellous <olowomarve@gmail.com>
2021-05-09 17:54:34 -1100

    fixed tinder cards

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import TinderCard from 'react-tinder-card';
 import './Tindercards.css';
+// import cardpic from './jeff_marvy.JPG'
 
 function TinderCards() {
     const [people] = useState([
@@ -19,24 +20,24 @@ function TinderCards() {
 
     return (
         <div className='tindercards'>
-        <div className='tinderCards__cardsContainer'>
-            {people.map((person) => {
-            <TinderCard
-                className='swipe'
-                key={person.name}
-                preventSwipe={['up', 'down']}
-                // onSwipe={(dir) => Swipe(dir, character.name)}
-                // onCardLeftScreen={() => outOfFrame(character.name)}
-            >
-                <div
-                style={{ backgroundImage: `url(${person.url})` }}
-                className='card'
-                >
-                <h3>{person.name}</h3>
-                </div>
-            </TinderCard>
-            })}
-        </div>
+            <div className='tinderCards__cardsContainer'>
+                {people.map((person) => 
+                    <TinderCard
+                        className='swipe'
+                        key={person.name}
+                        preventSwipe={['up', 'down']}
+                        // onSwipe={(dir) => Swipe(dir, character.name)}
+                        // onCardLeftScreen={() => outOfFrame(character.name)}
+                    >
+                        <div
+                        style={{ backgroundImage: `url(${person.url})` }}
+                        className='card'
+                        >
+                        <h3>{person.name}</h3>
+                        </div>
+                    </TinderCard>
+                )}
+            </div>
         </div>
     )
 }
