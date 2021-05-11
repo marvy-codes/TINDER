6920b3ccba39478aed26198d318b72b92b281cee
Olowookere Marvellous <olowomarve@gmail.com>
2021-05-11 09:35:29 -1100

    swipeButtons updated

--- src/SwipeButtons.js
+++ src/SwipeButtons.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import "./swipeButtons.css";
+import IconButton from "@material-ui/core/IconButton"; 
+import RelayIcon from "@material-ui/icons/Replay";
+import CloseIcon from '@material-ui/icons/Close';
+import StarRateIcon from '@material-ui/icons/StarRate';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FlashOnIcon from '@material-ui/icons/FlashOn';
 
 function SwipeButtons() {
     return (
-        <div className="swipeButtons ">
-            
+        <div className='swipeButtons'>
+            <IconButton className="swipeButtons_repeat">
+                <RelayIcon fontSize="large" />
+            </IconButton>
+
+            <IconButton className="swipeButtons_left">
+                <CloseIcon fontSize="large"/>
+            </IconButton>
+
+            <IconButton className="swipeButtons_star">
+                <StarRateIcon fontSize="large"/>
+            </IconButton>
+
+            <IconButton className="swipeButtons_right">
+                <FavoriteIcon fontSize="large" />
+            </IconButton>
+
+            <IconButton className="swipeButtons_lightning">
+                <FlashOnIcon fontSize="large" />
+            </IconButton>
         </div>
     )
 }
